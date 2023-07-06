b7b3bd68bf8a616c71a6babe9bef8c0d98dca6c9
msatchell1 <112362230+msatchell1@users.noreply.github.com>
2023-07-06 12:59:17 -0400

    update

--- MATLAB/js_SingleDayExpt/raster_explore.m
+++ MATLAB/js_SingleDayExpt/raster_explore.m
@@ -5,3 +5,38 @@
 
 % Maybe I can also look for theta sequences during the task performance
 
+
+%% Load data
+
+data_dir = '/mnt/10TBSpinDisk/js_SingleDayExpt'; % Location of data for all rats
+% For this analysis I want to consider all rats, and all nrns on all
+% tetrodes for each rat.
+load_rats = {'ZT2','ER1_NEW','KL8','BG1','JS14','JS15','JS17','JS21','JS34'};
+
+% Mean firing rate data for each nrn is held in the cellinfo file.
+filetype = {'linpos01','linfields01'};
+
+C_alldata = {}; % Cell array to hold data for each rat. If multiple filetypes 
+% are loaded, each row holds a different file type, ordered in the same
+% order as the elements in filetype.
+
+for r = 1:length(load_rats)
+
+    for ft = 1:length(filetype)    
+
+        % Does not load from EEG folder
+        File_dir = dir(data_dir+"/"+load_rats(r)+'_direct'+"/*"+filetype{ft}+"*");
+    
+        if isempty(File_dir)
+            error("%s file does not exist for animal: %s \n",filetype{ft},load_rats{r})
+        elseif length(File_dir) > 1
+            error("More than one file detected when searching for: %s, in animal: %s \n" + ...
+                "Must load only one file per element in filetype.", filetype{ft},load_rats{r})
+        else
+            file = struct2cell(load(string(fullfile(File_dir.folder, File_dir.name)))); % load data
+            file = file{:};
+            C_alldata{ft,r} = file{1,1};
+            fprintf("Loaded animal: %s      file: %s  \n", load_rats{r}, File_dir.name)
+        end
+    end
+end
