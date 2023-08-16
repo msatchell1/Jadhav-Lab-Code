c196d4f7449b1f9dec55966f9e51d6b42f9b2bbb
msatchell1 <112362230+msatchell1@users.noreply.github.com>
2023-08-16 17:23:19 -0400

    update

--- MATLAB/js_SingleDayExpt/ripple_explore.m
+++ MATLAB/js_SingleDayExpt/ripple_explore.m
@@ -170,7 +170,7 @@ restEpochs = 1:2:17;
 % I should also order the place cells based on peak firing location along
 % one (or all) of the trajectories.
 r = 2; % rat number
-eStr = "03"; % epoch number
+eStr = "06"; % epoch number
 riptetsStr = {'26','25','24','23', '11','07','21','08','15','13', '14'}; % tetrode to take LFP from, ideally displays most of the ripples
 e = str2num(eStr);
 riptets = [26,25,24,23, 11,7,21,8,15,13, 14]; % ENSURE this matches riptetsStr
@@ -200,17 +200,26 @@ for rt = 1:length(riptets)
 end
 
 nrnTets = [26,25,24,23, 11,7,21,8,15,13, 14]; % Tetrodes to plot neurons from.
+nrnCount = 0; % To count the number of plotted neurons in the raster for spacing purposes.
 for i = 1:length(nrnTets)
     tet = nrnTets(i);
-    nrns = [C_allspikes{1,r}{1,e}{1,tet}{:}];
-    idx = find(riptets == tet);
+    nrns = [C_allspikes{1,r}{1,e}{1,tet}{:}]; % struct with rows of neurons.
+    if ~isempty(nrns)
+        meanRates = [nrns.meanrate]';
+        isI = meanRates > 7; % Inhibitory neurons.
+        nrns = nrns(~isI); % Remove inhibitory neurons from struct.
 
-    for j = 1:length(nrns)
-        nrn = nrns(j);
-        spikeTimes = nrn.data(:,1);
-        
-        yvals = zeros(size(spikeTimes)) + (min(ydisp)-max(ydisp) + ydisp(idx)-0.5*(j-1) - 2);
-        plot(spikeTimes, yvals, Color=tetcolors(idx,:), Marker="|", LineStyle="none")
+        idx = find(riptets == tet);
+    
+        for j = 1:length(nrns)
+            nrn = nrns(j);
+            spikeTimes = nrn.data(:,1);
+            
+            yvals = zeros(size(spikeTimes)) + (min(ydisp)-2-0.5*nrnCount);
+            plot(spikeTimes, yvals, Color=tetcolors(idx,:), Marker="|", LineStyle="none")
+
+            nrnCount = nrnCount + 1;
+        end
     end
 end
 
@@ -218,8 +227,8 @@ end
 riptData = C_allript{1,r}{1,e};
 for i = 1:length(riptData.starttime)
     x_vertices = [riptData.starttime(i),riptData.endtime(i),riptData.endtime(i),riptData.starttime(i)];
-    y_vertices = [min(3*min(ydisp)-max(ydisp)),min(3*min(ydisp)-max(ydisp)),max(refV),max(refV)];
-    patch(x_vertices, y_vertices, [0.4940 0.1840 0.5560],'FaceAlpha', 0.4, 'EdgeColor','none')
+    y_vertices = [min(ydisp)-2-0.5*nrnCount,min(ydisp)-2-0.5*nrnCount,max(refV),max(refV)];
+    patch(x_vertices, y_vertices, [0.4940 0.1840 0.5560],'FaceAlpha', 0.2, 'EdgeColor','none')
 end
 title(sprintf("Rat %d, Epoch %d",r,e))
 ylabel("Voltage (mV)")
